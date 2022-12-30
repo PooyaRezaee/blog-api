99cacaf9df0fa68f67ced17a6adf51106b1b209e
PooyaRezaee <pooya.rezaee.official@gmail.com>
2022-12-30 23:57:55 +0330

    Added Count Like Article To Admin Panel

--- apps/article/admin.py
+++ apps/article/admin.py
@@ -3,8 +3,11 @@ from .models import Article
 
 @admin.register(Article)
 class ArticleModelAdmin(admin.ModelAdmin):
-    list_display = ('title','author','created')
+    list_display = ('title','author','like_count','created')
     list_filter = ('created','title','author')
     search_fields = ('title','body')
     ordering = ('created',)
     exclude = ('like',)
+
+    def like_count(self, obj):
+        return obj.like.all().count()
